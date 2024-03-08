a4e1e8e3034e089d33ced87737cea0327cf8730c
SJD39 <sanjiudao@qq.com>
2024-03-08 23:35:24 +0800

    初始代码

---
+++ content-scripts/content.js
@@ -0,0 +1,165 @@
+class OSUCursor {
+    constructor() {
+        // 创建html元素
+        this.cursorBox = document.createElement('div');
+        this.cursorCenter = document.createElement('div');
+        this.cursorRing = document.createElement('div');
+        this.cursorCenterImg = document.createElement('img');
+        this.cursorRingImg = document.createElement('img');
+        this.cursorCanvas = document.createElement('canvas');
+
+        this.cursorBox.id = 'cursorBox';
+        this.cursorCenter.id = 'cursorCenter';
+        this.cursorRing.id = 'cursorRing';
+        this.cursorCanvas.id = 'cursorCanvas';
+
+        this.cursorCanvas.width = window.innerWidth;
+        this.cursorCanvas.height = window.innerHeight;
+
+        this.cursorCenterImg.src = chrome.runtime.getURL('./content-scripts/img/cursormiddle@2x.png');
+        this.cursorRingImg.src = chrome.runtime.getURL('./content-scripts/img/cursor@2x.png');
+
+        this.cursorBox.append(this.cursorCenter, this.cursorRing);
+        this.cursorCenter.append(this.cursorCenterImg);
+        this.cursorRing.append(this.cursorRingImg);
+
+        document.getElementsByTagName('body')[0].append(this.cursorCanvas, this.cursorBox);
+
+        // 初始化Canvas
+        this.ctx = this.cursorCanvas.getContext("2d");
+        this.ctx.lineCap = 'round';
+        this.ctx.lineJoin = 'round';
+        this.ctx.strokeStyle = '#88bbff';
+        this.ctx.lineWidth = 4;
+        // this.ctx.shadowBlur = 4;
+        // this.ctx.shadowColor = '#88bbff';
+
+        // 初始化变量
+        this.tracePoints = [];
+        this.traceLength = 40;
+    }
+
+    // 更新鼠标数据
+    addPoint(x, y, t) {
+        this.tracePoints.unshift([x, y, t]);
+        if (this.tracePoints.length > this.traceLength) {
+            this.tracePoints.length = this.traceLength;
+        }
+        return;
+    }
+
+    // 设置指针位置
+    setCursorPoint(x, y) {
+        this.cursorBox.style.top = y + 'px';
+        this.cursorBox.style.left = x + 'px';
+        return;
+    }
+
+    renewCursorPoint() {
+        if (this.tracePoints.length == 0) {
+            return;
+        }
+        this.setCursorPoint(this.tracePoints[0][0], this.tracePoints[0][1]);
+        return;
+    }
+
+    // 指针可见
+    visibleCursor() {
+        this.cursorBox.style.opacity = 1;
+        return;
+    }
+
+    // 指针不可见
+    invisibleCursor() {
+        this.cursorBox.style.opacity = 0;
+        return;
+    }
+
+    // 指针变大
+    cursorToBig() {
+        this.cursorRingImg.style.animation = 'CursorRingBig .2s forwards, CursorRingRotate 3s linear infinite';
+        return;
+    }
+
+    // 指针变小
+    cursorToSmall() {
+        this.cursorRingImg.style.animation = 'CursorRingSmall .2s forwards, CursorRingRotate 3s linear infinite';
+        return;
+    }
+
+    animationLoop() {
+        if (this.tracePoints.length == 0) {
+            window.requestAnimationFrame(this.animationLoop.bind(this));
+            return;
+        }
+
+        // 更新光标位置
+        this.renewCursorPoint();
+
+        // 绘制轨迹
+        let time = new Date().getTime();
+
+        // 清空画布
+        this.ctx.clearRect(0, 0, this.cursorCanvas.width, this.cursorCanvas.height);
+
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.tracePoints[0][0], this.tracePoints[0][1]);
+        for (let i = 1; i < this.tracePoints.length; i++) {
+            this.ctx.lineTo(this.tracePoints[i][0], this.tracePoints[i][1]);
+            if (time - this.tracePoints[i][2] > 800) {
+                this.ctx.globalAlpha = 0;
+                this.ctx.stroke();
+                break;
+            } else {
+                this.ctx.globalAlpha = 1 - ((time - this.tracePoints[i - 1][2]) / 800);
+            }
+
+            this.ctx.stroke();
+        }
+        this.ctx.closePath();
+
+        window.requestAnimationFrame(this.animationLoop.bind(this));
+
+    }
+
+}
+
+var CUR;
+
+CUR = new OSUCursor();
+
+CUR.animationLoop();
+
+window.addEventListener('mousemove', function (e) {
+    CUR.visibleCursor();
+    CUR.addPoint(e.clientX, e.clientY, new Date().getTime());
+    return;
+});
+
+window.addEventListener('mouseover', function (e) {
+    if (e.fromElement != null && e.fromElement.localName != 'iframe') {
+        return;
+    }
+
+    CUR.visibleCursor();
+    return;
+});
+
+window.addEventListener('mouseout', function (e) {
+    if (e.toElement != null && e.toElement.localName != 'iframe') {
+        return;
+    }
+
+    CUR.invisibleCursor();
+    return;
+});
+
+window.addEventListener('mousedown', function () {
+    CUR.cursorToBig();
+    return;
+});
+
+window.addEventListener('mouseup', function () {
+    CUR.cursorToSmall();
+    return;
+});
